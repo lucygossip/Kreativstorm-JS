23f0f3046692cd5a5c59c148e82230532f723f59
GlindaRose <45208607+GlindaRose@users.noreply.github.com>
2023-08-04 10:02:19 +0100

    Fixes to playRound and game functions, plus alerts and console.logs to help player navigate the game

--- script.js
+++ script.js
@@ -1,64 +1,86 @@
-//Create computerPlay function
-
-function computerPlay() {
-    let rockPaperScissors = ["rock", "paper", "scissors"]; // Create rock, paper and scissors
-    return rockPaperScissors[(Math.floor(Math.random() * rockPaperScissors.length))]; //Returns random option from options array
-}
-
-//console.log(computerPlay());
-//function that plays the first round of the game
+let rockPaperScissors = ["rock", "paper", "scissors"]; // Create rock, paper and scissors
 
 //make variables globle to use it on game function
 let playerwin = 0;
 let computerwin = 0;
-let faildprompt = false;
 
-let PlayRound = (playerSelection, computerPlay)=> {
+//Create computerPlay function
+function computerPlay() {
+    return rockPaperScissors[(Math.floor(Math.random() * rockPaperScissors.length))]; //Returns random option from options array
+}
+
+//console.log(computerPlay());
+
+//function that plays the first round of the game
+
+let playRound = (playerSelection, computerPlay)=> {
+    if(!playerSelection) {
+        alert("Please don't leave, I wanna play!");
+        return false;
+    }
     // changing user input not be case sentsitive depending on how string is entered
-    playerSelection.toLowerCase();
+   playerSelection =  playerSelection.toLowerCase();
+    console.log(`Player has chosen ${playerSelection} and computer has chosen ${computerPlay}`);
     if (playerSelection===computerPlay){
-        // console.log(`Playerselection: ${playerSelection} and computerPlayState: ${computerPlay} =Draw`);
-        return (`Playerselection: ${playerSelection} and computerPlayState: ${computerPlay}= Draw`);
+        return "Draw"
     }
     else if(playerSelection==='rock' && computerPlay==='paper'){
-        computerwin++;
-        return (`Playerselection: ${playerSelection} and, computerPlay:${computerPlay} =  Computer win`);
+        return "Computer wins"
     }
 
     else if(playerSelection==='paper' && computerPlay==='rock'){
-        playerwin++;
-        return (`Playerselection: ${playerSelection} and, computerPlay:${computerPlay} =  Player win`);
+        return "Player wins"
     }
 
     else if(playerSelection==='paper' && computerPlay==='scissors'){
-        computerwin++;
-        return(`Playerselection: ${playerSelection} and, computerPlay:${computerPlay} =  Computer win`)
+        return "Computer wins"
     }
 
     else if(playerSelection==='scissors' && computerPlay==='paper'){
-        playerwin++;
-        return(`Playerselection: ${playerSelection} and, computerPlay:${computerPlay} =  Player win`)
+        return "Player wins"
     }
-
     else if(playerSelection==='scissors' && computerPlay==='rock'){
-        computerwin++;
-        return(`Playerselection: ${playerSelection} and, computerPlay:${computerPlay} =  Computer win`)
+        return "Computer wins"
     }
     else if(playerSelection==='rock' && computerPlay==='scissors'){
-        playerwin++;
-        return(`Playerselection: ${playerSelection} and, computerPlay:${computerPlay} = Player win`)
+        return "Player wins"
     }
     else{
-        faildprompt = true;
+        return false;
         alert('please choose between rock, paper and scissors');
     };
 };
 
+//const playerSelection = "paper";
+//const computerSelection = computerPlay();
+//console.log(playRound(playerSelection, computerSelection));
+
+const game = () => {
+    for(let i = 0; i < 5; i++) {
+        const result = playRound(prompt('Please enter rock, paper or scissors.'), computerPlay());
+        if(result){
+            console.log(`${result} | Current score: Player - ${playerwin}, Computer - ${computerwin}`);
+            if(result == "Player wins") {
+                alert("You win!");
+                playerwin++
+            } else {
+                alert("You lose!");
+                computerwin++;
+            }
+        } else i--;
+        if(playerwin == 3){alert("You Won the GAME!"); return;}
+        if(computerwin == 3){alert("You Lost the GAME!"); return;}
+    }
+}
+
+alert("Welcome to Rock, Paper, Scissors! To begin the game, please open the console and type game()")
+
+/*
 let game = () =>{
     //loop to count 5 rounds 
     for(let i = 0;i < 5;i++) {
-       //get input from user and call playround function
-       let result = PlayRound(prompt('Please enter any string value: '),computerPlay());
+       //get input from user and call playRound function
+       const result = playRound(prompt('Please enter any string value: '),computerPlay());
        //if no typo the round will count
        if(!faildprompt){
         console.log(result);
@@ -78,4 +100,5 @@ let game = () =>{
     }
 }
 // call the game function
-game();
+
+*/

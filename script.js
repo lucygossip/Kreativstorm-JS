1ed2b99750547fba5a2bb74fe3b552fbe6ae5e9e
Irfan Ishtiaq <irfanbajwa34@gmail.com>
2023-08-05 00:43:38 +0500

    A new change in logic

--- script.js
+++ script.js
@@ -12,46 +12,44 @@ let playRound = (playerSelection, computerPlay) => {
     }
     playerSelection = playerSelection.toLowerCase();
     computerPlay = computerPlay.toLowerCase();
-    console.log(`Player has chosen ${playerSelection} and computer has chosen ${computerPlay}`);
 
-    let result;
     if (playerSelection === computerPlay) {
-        result = "Draw";
+        return "Draw";
     } else if (
         (playerSelection === 'rock' && computerPlay === 'scissors') ||
         (playerSelection === 'scissors' && computerPlay === 'paper') ||
         (playerSelection === 'paper' && computerPlay === 'rock')
     ) {
         playerwin++;
-        result = `You Win! ${capitalize(playerSelection)} beats ${capitalize(computerPlay)}`;
+        return `You Win! ${capitalize(playerSelection)} beats ${capitalize(computerPlay)}`;
     } else if (
         playerSelection === 'rock' || playerSelection === 'paper' || playerSelection === 'scissors'
     ) {
         computerwin++;
-        result = `You Lose! ${capitalize(computerPlay)} beats ${capitalize(playerSelection)}`;
+        return `You Lose! ${capitalize(computerPlay)} beats ${capitalize(playerSelection)}`;
     } else {
         alert('Please choose between rock, paper, and scissors');
         return false;
     }
-    alert(result);
-    return result;
 };
 
 const game = () => {
-    for (let i = 0; i < 5; ) { // Removed incrementation from here
+    for (let i = 0; i < 5; ) {
         const result = playRound(prompt('Please enter rock, paper or scissors. (or cancel to quit)'), computerPlay());
         if (result === "exit") {
             alert("Thanks for playing! Hope to see you again.");
             return;
         } else if (result === "Draw") {
             alert("It's a draw! Let's replay the round.");
+            continue; // Continue to the next iteration without incrementing
         } else if (result) {
+            alert(result);
             console.log(`${result} | Current score: Player - ${playerwin}, Computer - ${computerwin}`);
-            i++; // Increment only if the round was not a draw
+            i++;
         }
         if (playerwin == 3 || computerwin == 3) {
             alert(playerwin == 3 ? "You Won the GAME!" : "You Lost the GAME!");
-            resetGame(); // Reset the game
+            resetGame();
             return;
         }
     }
